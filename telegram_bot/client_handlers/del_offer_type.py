a7be938ac54e9840e6dc02d9215368f147f272c9
jakefish18 <jakefish18@yandex.ru>
2023-03-03 12:55:32 +0500

    Adding keyboard for delete command

--- telegram_bot/client_handlers/del_offer_type.py
+++ telegram_bot/client_handlers/del_offer_type.py
@@ -6,7 +6,7 @@ from aiogram.dispatcher.storage import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
 from telegram_bot.init_bot import bot, bot_dispatcher, users_handler, offer_type_items_handler, user_offer_types_handler
-
+from telegram_bot.client_handlers.reply_markups import generate_markup
 
 DEL_OFFER_TYPE_RESPONSE_1 = "Чтобы удалить добавленный товар, введите название этого товара:"
 DEL_OFFER_TYPE_ERROR_2 = "Ошибка! Вы не добавляли такого товара!"
@@ -23,8 +23,21 @@ async def del_offer_type_st1(query: types.CallbackQuery):
     user executes del_offer_type.
     """
     user_telegram_id = query.from_user.id
+
+    # Generating reply markup keyboard.
+    user_id = users_handler.get_user_id(user_telegram_id)
+    user_offer_type_ids = user_offer_types_handler.get_user_offer_types(user_id)
+
+    user_offer_type_titles = []
+
+    for user_offer_type_id in user_offer_type_ids:
+        user_offer_type_title = offer_type_items_handler.get_offer_type_title(user_offer_type_id)
+        user_offer_type_titles.append([user_offer_type_title])
+
+    kbm_user_offer_type_titles = generate_markup(user_offer_type_titles)
+
     await DelOfferTypeForm.input_item.set()
-    await bot.send_message(user_telegram_id, DEL_OFFER_TYPE_RESPONSE_1)
+    await bot.send_message(user_telegram_id, DEL_OFFER_TYPE_RESPONSE_1, reply_markup=kbm_user_offer_type_titles)
 
 async def del_offer_type_st2(message: types.message, state: FSMContext):
     """
--- telegram_bot/client_handlers/help.py
+++ telegram_bot/client_handlers/help.py
@@ -16,5 +16,13 @@ async def help(message: types.message):
     user_telegram_id = message.from_user.id
     await bot.send_message(user_telegram_id, HELP_COMMAND_RESPONSE, reply_markup=kbm_main_menu)
 
+async def help_callback(query: types.CallbackQuery):
+    """
+    Sending help info to the user.
+    """
+    user_telegram_id = query.from_user.id
+    await bot.send_message(user_telegram_id, HELP_COMMAND_RESPONSE, reply_markup=kbm_main_menu)
+
 def register_help_command(bot_dispatcher: Dispatcher):
     bot_dispatcher.register_message_handler(help, commands=["help"])
+    bot_dispatcher.register_callback_query_handler(help_callback, text="help")
